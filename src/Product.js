92eaa91d1ab5bc4590b98592d3d309cc53726f39
AadhilFarhan <aadhilfarhan30@gmail.com>
2022-02-20 10:58:50 +0530

    .

---
+++ src/Product.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return <div className="product"></div>;
+}
+
+export default Product;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCHndNQE9_g3oSKzqzqZezvgbDUrIO8lwk",
+    authDomain: "challenge-4ff4d.firebaseapp.com",
+    projectId: "challenge-4ff4d",
+    storageBucket: "challenge-4ff4d.appspot.com",
+    messagingSenderId: "490442080693",
+    appId: "1:490442080693:web:c92b495a3867a5e9f359fc",
+    measurementId: "G-6DM83HPC6B"
+  };
