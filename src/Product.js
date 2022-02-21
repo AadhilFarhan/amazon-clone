30151e2a99f0afb15f1b19f69c1583057fe25e95
AadhilFarhan <aadhilfarhan30@gmail.com>
2022-02-21 23:43:30 +0530

    change 2 in product

--- src/Product.js
+++ src/Product.js
@@ -10,6 +10,13 @@ function Product() {
           <small>$</small>
           <strong>19.99</strong>
         </p>
+        <div className="product__rating">
+          <p>⭐</p>
+          <p>⭐</p>
+          <p>⭐</p>
+          <p>⭐</p>
+          <p>⭐</p>
+        </div>
       </div>
     </div>
   );
