22261710cc67dfd79ee7bab2f67680db5bd06ff9
AadhilFarhan <aadhilfarhan30@gmail.com>
2022-02-23 08:51:22 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -13,6 +13,16 @@ function Home() {
         />
 
         <div className="home__row">
+          <Product
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
           <Product />
           <Product />
         </div>
