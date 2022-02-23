58775b4f0eb572a110592d10ac4c0bb08f8f1793
AadhilFarhan <aadhilfarhan30@gmail.com>
2022-02-23 08:08:48 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -14,6 +14,7 @@ function Home() {
 
         <div className="home__row">
           <Product />
+          <Product />
         </div>
       </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -6,7 +6,7 @@ function Product() {
     <div className="product">
       <div className="product__info">
         <p>The lean startup</p>
-        <p className="product__rise">
+        <p className="product__price">
           <small>$</small>
           <strong>19.99</strong>
         </p>
