4de3eb86bdde396b69777051c3ad950a4b911649
AadhilFarhan <aadhilfarhan30@gmail.com>
2022-02-24 22:17:02 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -14,17 +14,42 @@ function Home() {
 
         <div className="home__row">
           <Product
+            id="12232212"
             title="The lean startup"
             price={29.99}
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
             rating={5}
           />
-          <Product />
+          <Product
+            id="32212313"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+            rating={5}
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="2222211"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            rating={5}
+          />
+          <Product
+            id="12631231"
+            title="Amazon Echo(3rd Generaton)  | Smart Speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+            rating={5}
+          />
+          <Product
+            id="2431231231"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+            rating={5}
+          />
         </div>
       </div>
     </div>
