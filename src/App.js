4895699319cab55da4ad3bffe75ce65a3a645547
AadhilFarhan <aadhilfarhan30@gmail.com>
2022-03-07 00:10:22 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
