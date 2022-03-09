69006ac71b644597c4571a728d66c7ecbed75142
AadhilFarhan <aadhilfarhan30@gmail.com>
2022-03-09 23:56:16 +0530

    ..

--- src/Checkout.js
+++ src/Checkout.js
@@ -10,6 +10,12 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
+        <div>
+          <h2 className="checkout__title">Your shopping basket</h2>
+        </div>
+      </div>
+      <div className="checkout__right">
+        <h2>The subtotal will go here</h2>
       </div>
     </div>
   );
