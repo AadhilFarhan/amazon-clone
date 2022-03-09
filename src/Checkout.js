ecbd329c4e8f80b3dbd87e97cdda4484ba6b7a6d
AadhilFarhan <aadhilfarhan30@gmail.com>
2022-03-09 23:50:02 +0530

    .

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,13 @@ import "./Checkout.css";
 function Checkout() {
   return (
     <div className="checkout">
-      <h1>CHECKOUT</h1>
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
